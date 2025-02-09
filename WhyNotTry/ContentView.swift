714bfd41dbf9f5da225f76df084ed2a25cd583b2
Sajid <sajidahmed.reshmi@outlook.com>
2025-02-09 01:12:48 +0100

    first commit

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -10,53 +10,49 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
 
+
+    @State private var selected = "Archery"
+    
+    @State private var id = 1
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        Text("Why not try...")
+            .font(.largeTitle.bold())
+        
+        Spacer()
+        
+        VStack {
+            
+            Circle()
+                .fill(colors.randomElement() ?? .blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            
+            Text("\(selected)!")
+                .font(.title)
+            
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+        .transition(.slide)
+        .id(id)
+        
+        Spacer()
+        
+        Button("Try again") {
+            withAnimation(.easeInOut(duration: 1)) {
+                selected = activities.randomElement() ?? "Archery"
+                id += 1
             }
         }
+        .buttonStyle(.borderedProminent)
     }
 }
 
